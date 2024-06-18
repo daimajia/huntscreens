7509205dace641f705aef6856ef229cef502c9be
daimajia <daimajia@mail.bnu.edu.cn>
2024-06-18 11:52:25 +0800

    fix cache issue when prod.

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,20 @@ import { db } from '@/db/db';
 import { Producthunt, producthunt } from '@/db/schema/ph';
 import MiniScreenshotCard from './components/screenshot.card';
 import { desc } from 'drizzle-orm';
+import { cache } from 'react';
 
-async function getPHPost() {
+export const revalidate = 60;
+
+const getPHPosts = cache(async () => {
   const data = await db.query.producthunt.findMany({
     orderBy: [desc(producthunt.added_at)],
     limit: 30
-  });
+  })
   return data as Producthunt[];
-}
+});
 
 export default async function Home() {
-  const phs = await getPHPost();
+  const phs = await getPHPosts();
   return (
     <main className="flex flex-col items-center justify-between p-5 md:p-10 gap-10">
       <div className=' w-full'>
