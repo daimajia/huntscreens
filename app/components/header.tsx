269a57649b8763b9571a44f5158654bf89ea56f8
daimajia <daimajia@mail.bnu.edu.cn>
2024-07-04 04:38:38 +0800

    add github link.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,6 +4,9 @@ import { logtoConfig } from "../logto";
 import { getLogtoContext, signIn } from "@logto/next/server-actions";
 import SignIn from "./sign-in";
 import UserMenu from "./user.menu";
+import { Button } from "@/components/ui/button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { GithubIcon, LucideGithub } from "lucide-react";
 
 export default async function Header() {
   const { isAuthenticated, userInfo } = await getLogtoContext(logtoConfig, { fetchUserInfo: true });
@@ -11,8 +14,8 @@ export default async function Header() {
     <div className="flex flex-row justify-between py-4 px-4 md:px-10  sticky top-0 z-50 border-b dark:border-none  navbar bg-base-100  bg-background">
       <div className="bg-dark-logo">
         <Link href="/">
-          <img loading="lazy"  src="/logo.png" className="dark:hidden w-40"></img>
-          <img loading="lazy"  src="/dark-logo.png" className="w-0 dark:w-40"></img>
+          <img loading="lazy" src="/logo.png" className="dark:hidden w-40"></img>
+          <img loading="lazy" src="/dark-logo.png" className="w-0 dark:w-40"></img>
         </Link>
       </div>
       <div className="flex flex-row gap-4 items-center justify-center">
@@ -22,7 +25,12 @@ export default async function Header() {
         {!isAuthenticated && <SignIn onSignIn={async () => {
           'use server';
           await signIn(logtoConfig);
-        }}/>}
+        }} />}
+        <Link title="Looking forward to your contribution.❤️" href="https://github.com/daimajia/huntscreens" target="__blank">
+          <Button size={"icon"} variant={"outline"}>
+            <GitHubLogoIcon className="w-4 h-4" />
+          </Button>
+        </Link>
         <ModeToggle />
       </div>
     </div>
