68ac83535e2db21b50db8edba10471a80aa468b7
daimajia <daimajia@mail.bnu.edu.cn>
2024-07-07 05:01:16 +0800

    add metadata to the detailed product.

--- app/p/[id]/page.tsx
+++ app/p/[id]/page.tsx
@@ -1,8 +1,46 @@
 import ProductDetailPage from "@/app/components/product.detail";
 import queryProduct from "@/lib/api/query.product";
 import { cookies } from "next/headers";
+import type { Metadata, ResolvingMetadata } from 'next';
+import { db } from "@/db/db";
+import { eq } from "drizzle-orm";
+import { producthunt } from "@/db/schema/ph";
 
-export default async function ProductDetail({ params }: { params: { id: number } }) {
+type Props = {
+  params: { id: number }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const product = await db.query.producthunt.findFirst({
+    where: eq(producthunt.id, params.id)
+  });
+  return {
+    title: `${product?.name} - ${product?.tagline}`,
+    description: product?.description,
+    publisher: "huntscreens.com",
+    openGraph: {
+      title: `${product?.name}`,
+      description: `${product?.tagline} - ${product?.description}`,
+      publishedTime: `${product?.featuredAt}`,
+      images: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2}/${product?.uuid}.webp`
+    },
+    keywords: product?.topics?.nodes.flatMap(item => item.name),
+    twitter: {
+      title: `${product?.name} - from huntscreens.com`,
+      description: `${product?.tagline}`,
+      images: {
+        url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2}/${product?.uuid}.webp`,
+        alt: `${product?.name} screenshot`
+      }
+    }
+  }
+}
+
+
+export default async function ProductDetail({ params }: Props) {
   const sort = cookies().get('sort')?.value || 'time';
   const data = await queryProduct(params.id, sort === "time" ? "time" : "vote");
   return <>
