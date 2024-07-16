69e5709d0f096cac89ccccf23e4bf9f365ec78e5
daimajia <daimajia@mail.bnu.edu.cn>
2024-07-16 10:09:46 +0800

    upgrade NextJs to 14.2.5
    try to fix next/Link stuck issue.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // and the following to enable top-level await support for Webpack
-  experimental: {
-    serverActions: true,
-  },
   webpack: (config) => {
     return config;
   },
