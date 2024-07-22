3cac2483fec478436841953e26d5ac499639aea3
daimajia <daimajia@mail.bnu.edu.cn>
2024-07-22 14:54:49 +0800

    fix overflow when in small screen.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -25,28 +25,28 @@ export default async function Header() {
   return <>
     <div className="grid grid-cols-2 md:grid-cols-3 justify-between py-4 px-4 md:px-10  sticky top-0 z-50 border-b dark:border-none  navbar bg-base-100  bg-background">
 
-      <div className="hidden lg:flex flex-row gap-5 items-center text-sm ">
+      <div className="hidden lg:flex flex-row gap-5 items-center text-sm">
 
         <div className="flex flex-row justify-center items-center gap-1">
-          <Link href="/" className="hover:underline relative flex flex-row gap-1 justify-center items-center">
+          <Link href="/" title="ProductHunt" className="hover:underline relative flex flex-row gap-1 justify-center items-center">
             <img src="/phlogo.png" alt="" className=" h-10" />
-            ProductHunt
+            <span className="hidden 2xl:flex">ProductHunt</span>
           </Link>
           {updateCns.PH > 0 && <span title={`${updateCns.PH} new product today`} className=" bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">+{updateCns.PH}</span>}
         </div>
 
         <div className="flex flex-row justify-center items-center gap-1">
-          <Link href="/indiehackers" className="hover:underline relative flex flex-row gap-1 justify-center items-center">
+          <Link href="/indiehackers" title="IndieHackers" className="hover:underline relative flex flex-row gap-1 justify-center items-center">
             <img src="/indiehackers.jpg" alt="" className=" h-7 rounded-full" />
-            IndieHackers
+            <span className="hidden 2xl:flex">IndieHackers</span>
           </Link>
           {updateCns.Indiehackers > 0 && <span title={`${updateCns.Indiehackers} new hackers today`} className=" bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">+{updateCns.Indiehackers}</span>}
         </div>
 
         <div className="flex flex-row items-center justify-center gap-1">
-          <Link href="/startup/yc" className="hover:underline relative flex flex-row gap-1 justify-center items-center">
+          <Link href="/startup/yc" title="Y Combinator" className="hover:underline relative flex flex-row gap-1 justify-center items-center">
             <img src="/yc.png" alt="" className="h-7 rounded-full" />
-            Y Combinator
+            <span className="hidden 2xl:flex">YCombinator</span>
           </Link>
           {updateCns.YC > 0 && <span title={`${updateCns.YC} new company today`} className=" bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">+{updateCns.YC}</span>}
         </div>
