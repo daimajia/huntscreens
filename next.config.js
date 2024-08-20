33a1a52460daa0d9eca5c5ae8a8710f64785a244
daimajia <daimajia@mail.bnu.edu.cn>
2024-08-20 15:47:08 +0800

    change to npm
    because of the issue of the transformer.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+  },
   webpack: (config) => {
     return config;
   },
