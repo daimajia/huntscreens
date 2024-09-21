e4384c297c068b444b648c74d871e22437600d44
daimajia <daimajia@mail.bnu.edu.cn>
2024-09-21 10:07:39 +0800

    add metadata of diff product source.

--- db/schema/types.ts
+++ db/schema/types.ts
@@ -14,3 +14,30 @@ export interface PathSegment {
   slug_type: string;
   slug: string;
 }
+
+
+
+export type ProductHuntMetadata = {
+  votesCount: number;
+  commentCount: number;
+  featuredAt: Date;
+};
+
+export type IndieHackersMetadata = {
+  revenue: number;
+  followers: number;
+};
+
+export type YCMetadata = {
+  batch: string;
+  team_size: number;
+  launched_at: Date;
+  status: string;
+};
+
+export type TaaftMetadata = {
+  savesCount: number;
+  commentsCount: number;
+};
+
+export type ProductMetadata = ProductHuntMetadata | IndieHackersMetadata | YCMetadata | TaaftMetadata;
