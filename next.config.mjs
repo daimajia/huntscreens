e2e8a01450f62a943c77f71ed051d8a55509e156
daimajia <daimajia@mail.bnu.edu.cn>
2024-09-29 12:29:05 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   experimental: {
     serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
   },
